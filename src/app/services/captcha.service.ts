fe9b603d1bf25231ebbb38c51764d1c8d13aed13
kbar163 <kabarrantes163@gmail.com>
2022-10-19 10:10:04 -0600

    cors change

--- src/app/services/captcha.service.ts
+++ src/app/services/captcha.service.ts
@@ -17,7 +17,7 @@ export class CaptchaService {
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
       'Access-Control-Allow-Headers': 
-        'X-Requested-With, content-type'
+        'content-type'
     })
   };
   constructor(private http: HttpClient) {
